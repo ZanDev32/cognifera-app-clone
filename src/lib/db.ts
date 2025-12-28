9a322de350c4ed14a59c63d96543d4a190b38e24
ZanDev32 <52073144+ZanDev32@users.noreply.github.com>
2025-12-28 23:56:46 +0700

    fix Supabase connection refuse

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ 
+    connectionString,
 });
+
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
